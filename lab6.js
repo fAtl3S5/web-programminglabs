144e73847aac4473c483c694a5035dbb33d03b42
Студент Факультета Бизнеса <kopylov.kirill.1@mail.ru>
2022-12-20 09:18:04 +0700

    Alert

---
+++ lab6.js
@@ -0,0 +1,43 @@
+function setRussian() {
+    let name = document.getElementById('student');
+    name.innerHTML = 'Виктор Брюзгин';
+
+    let footer = document.getElementById('footer');
+    footer.innerHTML = 'Факультет бизнеса, ФБИ-02, 3 курс, 2022';
+
+    document.getElementById('your-name').innerHTML = 'Ваше имя';
+    document.getElementById('input-name').placeholder = 'введите имя';
+}
+
+function setEnglish() {
+    let name = document.getElementById('student');
+    name.innerHTML = 'Viktor Bryuzgin';
+
+    let footer = document.getElementById('footer');
+    footer.innerHTML = 'Faculty of Business, FBI-02, 3rd year, 2022';
+
+    document.getElementById('your-name').innerHTML = 'Your name';
+    document.getElementById('input-name').placeholder = 'enter your name';
+}
+
+function showAlert() {
+    let userName = document.getElementById('input-name').value;
+    let userSurname = document.getElementById('input-surname').value;
+    let userAge = document.getElementById('input-age').value;
+    alert('Привет, ' + userName + ' ' + userSurname + '\n' + 'Ваш возраст: ' + userAge);
+    let r = Math.round(Math.random()*255);
+    let g = Math.round(Math.random()*255);
+    let b= Math.round(Math.random()*255);
+    let color = 'rgb(' + r + ',' + g + ',' + b + ')';
+    document.getElementById('user-data').style.backgroundColor = color;
+}
+
+function showAlert2() {
+    alert('Это я делал');
+    document.getElementById('student').style.backgroundColor = 'red';
+}
+
+function showAlert3() {
+    alert('Это я делал');
+    document.getElementById('footer').style.backgroundColor = 'red';
+}
