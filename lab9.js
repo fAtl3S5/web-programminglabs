a91802f5b554bba1bec1cafe2eb9113270f65365
kkkkkk32 <90328946+kkkkkk32@users.noreply.github.com>
2023-01-03 16:08:21 +0700

    Add files via upload

---
+++ lab9.js
@@ -0,0 +1,35 @@
+function clickMe(button) {
+    if(button.innerHTML == 'Щелчок')
+        button.innerHTML = 'Успешно';
+    else
+        button.innerHTML = 'Щелчок';
+}
+
+
+function clickMe2(button) {
+    if(button.innerHTML == 'Коваль')
+        button.innerHTML = 'Глеб';
+    else
+        button.innerHTML = 'Коваль';
+}
+
+function clickMe3(button)
+{ 
+    if(button.innerHTML == 'Щелкни на меня')
+        button.innerHTML = 'Щелкни еще раз';
+    else if (button.innerHTML == 'Щелкни еще раз')
+        button.innerHTML = 'Ещё';
+        else if (button.innerHTML == 'Ещё')
+        button.innerHTML = 'Начать заного';
+    else
+        button.innerHTML = 'Нажми на меня';
+}
+
+function showCoords(event) {
+    event.target.innerHTML = 'x = ' + event.offsetX + ', y = ' + event.offsetY;
+}
+
+window.addEventListener('DOMContentLoaded', function() {
+    let coords = document.querySelector('#coords');
+    coords.addEventListener('mousemove', showCoords);
+});
