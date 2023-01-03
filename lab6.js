77325db9bbd9ca80f55a15b84736596c0ee4a764
kkkkkk32 <90328946+kkkkkk32@users.noreply.github.com>
2023-01-03 16:24:27 +0700

    Add files via upload

---
+++ lab6.js
@@ -0,0 +1,43 @@
+function setRussian() {
+    let name = document.getElementById('student');
+    name.innerHTML = 'Коваль Глеб';
+
+    let footer = document.getElementById('footer');
+    footer.innerHTML = 'Факультет бизнеса, ФБИ-02, 3 курс, 2022';
+
+    document.getElementById('your-name').innerHTML = 'Введите имя';
+    document.getElementById('input-name').placeholder = 'Введите имя';
+}
+
+function setEnglish() {
+    let name = document.getElementById('student');
+    name.innerHTML = 'Koval Gleb';
+
+    let footer = document.getElementById('footer');
+    footer.innerHTML = 'Facultate of Busiess, 3 course, FBI-02';
+
+    document.getElementById('your-name').innerHTML = 'Your name';
+    document.getElementById('input-name').placeholder = 'Enter your name';
+}
+
+function showAlert() {
+    let userName = document.getElementById('input-name').value;
+    let userSurname = document.getElementById('input-surname').value;
+    let userAge = document.getElementById('input-age').value;
+    alert('Hello, ' + userName + ' ' + userSurname + '\n' + 'Your age: ' + userAge);
+    let r = Math.round(Math.random()*255);
+    let g = Math.round(Math.random()*255);
+    let b= Math.round(Math.random()*255);
+    let color = 'rgb(' + r + ',' + g + ',' + b + ')';
+    document.getElementById('user-data').style.backgroundColor = color;
+}
+
+function showAlert2() {
+    alert('Посмотри на это!');
+    document.getElementById('student').style.backgroundColor = 'green';
+}
+
+function showAlert3() {
+    alert('Look at this!');
+    document.getElementById('footer').style.backgroundColor = 'green';
+}
